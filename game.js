2c184ec8e8dff2c48607782fa6bda6ed9b21b9b1
Daria Metelitsa <dariametelitsa@gmail.com>
2024-07-16 19:17:27 +0300

    added position generation

--- game.js
+++ game.js
@@ -1,26 +1,53 @@
 export class Game {
-#settings;
-#status = 'pending';
-#player1;
-#player2;
-#google;
+  #settings = {
+    gridSize: {
+      columns: 4,
+      rows: 4,
+    }
+  };
+  #status = "pending";
+  #player1;
+  #player2;
+  #google;
+
+  #getRandomPostion(takenPosition = []) {
+    let newX;
+    let newY;
+    do {
+      newX = NumberUtil.getRandomNumber(this.settings.gridSize.rows);
+      newY = NumberUtil.getRandomNumber(this.settings.gridSize.columns);
+    } while (
+      takenPosition.some(
+        (position) => position.x === newX && position.y === newY
+      )
+    );
+
+    return new Position(newX, newY);
+  }
+
+  get status() {
+    return this.#status;
+  }
 
   set settings(settings) {
     this.#settings = settings;
   }
-  get settings(){
+  get settings() {
     return this.#settings;
   }
 
-  start(){
+  start() {
+    if (this.#status === "pending") {
+      this.#status = "inProcess";
+    }
+    const player1Position = this.#getRandomPostion();
+    this.#player1 = new Player(1, player1Position);
+    this.#player2 = new Player(2, this.#getRandomPostion([player1Position.x, player1Position.y]));
   }
 
-  pause(){
-  }
+  pause() {}
 
-  resume(){
-  }
-
+  resume() {}
 }
 
 export class Player {
@@ -40,5 +67,11 @@ export class Position {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+}
+
+class NumberUtil {
+  static getRandomNumber(max) {
+    return Math.floor(Math.random() * max + 1);
   }
 }
--- game.test.js
+++ game.test.js
@@ -12,10 +12,25 @@ describe('Game test', () => {
         }
       }
   
-
     const settings = game.settings;
-
     expect(settings.gridSize.rows).toBe(10);
     expect(settings.gridSize.columns).toBe(10);
-  })
+  });
+
+  it('should change status', () => {
+    const game = new Game();
+
+    game.settings =
+      {
+        gridSize: {
+          columns: 10,
+          rows: 10,
+        }
+      }
+  
+
+    expect(game.status).toBe('pending');
+    game.start();
+    expect(game.status).toBe('inProcess');
+  });
 });
